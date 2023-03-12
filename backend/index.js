819b3b80bf28fcf1a3256005f8551096fab3eb95
Fredo <luisalfredo9905@gmail.com>
2023-03-12 17:50:10 -0500

    fix: call for windows platform

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ app.set('port', process.env.PORT || 8888)
 // Global middlewares
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: 'http://127.0.0.1:5173/',
+  credentials: true
+}));
 
 // Routes; this takes the index.js that is in the folder './routes'
 app.use("/api", require('./routes'))
